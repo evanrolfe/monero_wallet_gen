ccfcf1fcb900b2cfecb168dd48b162c1c16b6566
Evan Rolfe <evanrolfe@gmail.com>
2018-05-21 11:35:24 +0100

    remove vendor dir

--- vendor/bundle/ruby/2.3.0/gems/digest-sha3-1.1.0/ext/digest/extconf.rb
+++
@@ -1,7 +0,0 @@
-require 'mkmf'
-
-have_header('ruby/digest.h')
-have_func('rb_str_set_len')
-
-$CFLAGS << " -fvisibility=hidden"
-create_makefile('digest/sha3')
--- vendor/bundle/ruby/2.3.0/gems/digest-sha3-1.1.0/lib/digest/sha3/version.rb
+++
@@ -1,7 +0,0 @@
-module Digest
-  class SHA3
-    module Version
-      STRING = "1.1.0"
-    end
-  end
-end
